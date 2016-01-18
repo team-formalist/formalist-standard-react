23d61b1d9fd6b6161b94894fe57cd72540a1181f
Max Wheeler <max@makenosound.com>
2016-01-19 08:52:57 +1100

    Webpack compilation config. Redundant already, but wanted to note for posterity.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var path = require('path');
+var fs = require('fs');
+
+module.exports = {
+  entry: './lib/index.js',
+
+  output: {
+    filename: 'index.js',
+    path: path.resolve('./dist'),
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') }
+    ]
+  },
+
+  postcss: [
+    require('autoprefixer')
+  ],
+
+  resolve: {
+    modulesDirectories: ['node_modules']
+  },
+
+  plugins: [
+    new ExtractTextPlugin('index.css', { allChunks: true })
+  ]
+};
