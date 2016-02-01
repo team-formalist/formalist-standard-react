feb64bb11f5a57d7f4c4ec9978fea981433105c8
Max Wheeler <max@makenosound.com>
2016-02-01 13:46:09 +1100

    Use the `key` (which is the `path.hasCode()` of the position in the AST) to determine whether `Section` or `Group` components should re-render.

--- src/components/group.js
+++ src/components/group.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const Group = ({name, children}) => {
-  return (
-    <div className='fm-group'>
-      {children.map((child, index) => <div key={index} className='fm-group__item'>{child}</div>)}
-    </div>
-  )
+const Group = React.createClass({
+  shouldComponentUpdate (nextProps) {
+    // Use the path hash-code to determine whether or not to rerender this
+    // section. This should take account of any change to the AST.
+    // It will not account for changes to the overall form definition (but they
+    // should not change after runtime anyway)
+    return (this.props.key !== nextProps.key)
+  },
+
+  render () {
+    return (
+      <div className='fm-group'>
+        {children.map((child, index) => <div key={index} className='fm-group__item'>{child}</div>)}
+      </div>
+    )
+  }
 }
 
 export default Group
--- src/components/section.js
+++ src/components/section.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import ImmutablePropTypes from 'react-immutable-proptypes'
 import styles from './section.mcss'
 
-const Section = ({name, children}) => {
-  return (
-    <section className={styles.base}>
-      <h2 className={styles.name}>{name.replace(/_/, ' ')}</h2>
-      <div className={styles.children}>
-        {children}
-      </div>
-    </section>
-  )
-}
+const Section = React.createClass({
+  shouldComponentUpdate (nextProps) {
+    // Use the path hash-code to determine whether or not to rerender this
+    // section. This should take account of any change to the AST.
+    // It will not account for changes to the overall form definition (but they
+    // should not change after runtime anyway)
+    return (this.props.key !== nextProps.key)
+  },
+
+  render () {
+    let label = this.props.config.label || this.props.name.replace(/_/, ' ')
+    return (
+      <section className={styles.base}>
+        <h2 className={styles.name}>{label}</h2>
+        <div className={styles.children}>
+          {this.props.children}
+        </div>
+      </section>
+    )
+  }
+})
 
 export default Section
 export let SectionFactory = React.createFactory(Section)
