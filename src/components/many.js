45d317a5ab9abf5cf9e2e62e6bc2755bba4a9f6f
Max Wheeler <max@makenosound.com>
2016-02-25 14:17:41 +1100

    Add some boilerplate for the child-groups “sets” of a many.

--- src/components/many.js
+++ src/components/many.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
+const ManySet = React.createClass({
+  render () {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
 const Many = ({name, children, errors}) => {
   return (
     <div className='fm-many'>
       <h3 className='fm-many__name'>{name.replace(/_/, ' ')}</h3>
       <div className='fm-many__controls'><button>Add new {name.replace(/_/, ' ')}</button></div>
-      {children}
+      {children.map((setChildren) => (
+        <ManySet>
+          {setChildren}
+        </ManySet>
+      ))}
     </div>
   )
 }
