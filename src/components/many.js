a4027ce3f80ee76db6bd2c7cfd28508efc255a49
Max Wheeler <max@makenosound.com>
2016-03-10 11:36:14 +1100

    Bring `Many` into line with `better` AST changes.

--- src/components/many.js
+++ src/components/many.js
@@ -1,39 +1,58 @@
 import React from 'react'
+import ImmutablePropTypes from 'react-immutable-proptypes'
+import styles from './many.mcss'
 
 const ManySet = React.createClass({
-
   propTypes: {
     children: React.PropTypes.array
   },
 
   render () {
     return (
-      <div>
+      <div className={styles.set}>
         {this.props.children}
       </div>
     )
   }
 })
 
-const Many = ({name, children, errors}) => {
-  return (
-    <div className='fm-many'>
-      <h3 className='fm-many__name'>{name.replace(/_/, ' ')}</h3>
-      <div className='fm-many__controls'><button>Add new {name.replace(/_/, ' ')}</button></div>
-      {children.map((setChildren) => (
-        <ManySet>
-          {setChildren}
-        </ManySet>
-      ))}
-    </div>
-  )
-}
+const Many = React.createClass({
+  propTypes: {
+    hashCode: React.PropTypes.number.isRequired,
+    name: React.PropTypes.string,
+    type: React.PropTypes.string,
+    rules: ImmutablePropTypes.list,
+    errors: ImmutablePropTypes.list,
+    attributes: ImmutablePropTypes.map,
+    template: React.PropTypes.object,
+    children: ImmutablePropTypes.list
+  },
 
-Many.propTypes = {
-  name: React.PropTypes.string,
-  errors: React.PropTypes.array,
-  children: React.PropTypes.array
-}
+  shouldComponentUpdate (nextProps) {
+    // Use the path hash-code to determine whether or not to rerender this
+    // section. This should take account of any change to the AST.
+    // It will not account for changes to the overall form definition (but they
+    // should not change after runtime anyway)
+    return (this.props.hashCode !== nextProps.hashCode)
+  },
+
+  render () {
+    let { attributes, children, name } = this.props
+    let label = attributes.get('label') || name.replace(/_/, ' ')
+
+    return (
+      <div className={styles.base}>
+        <h3 className={styles.label}>{label}</h3>
+        <div className={styles.controls}><button>Add new {name.replace(/_/, ' ')}</button></div>
+        {children.map((setChildren) => (
+          <ManySet>
+            {setChildren}
+          </ManySet>
+        ))}
+      </div>
+    )
+  }
+})
 
 export default Many
 export let ManyFactory = React.createFactory(Many)
