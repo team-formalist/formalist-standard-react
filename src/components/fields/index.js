12d41bd789b95e2a309b430399551718764783b0
Max Wheeler <max@makenosound.com>
2016-03-15 16:12:06 +1100

    Change from underscore to camelCased values in the field definitions.

--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Container from './container'
-import CheckBox from './check_box'
-import DateField from './date_field'
-import DateTimeField from './date_time_field'
-import NumberField from './number_field'
-import RadioButtons from './radio_buttons'
-import SelectBox from './select_box'
-import TextField from './text_field'
-import TextArea from './text_area'
+import CheckBox from './check-box'
+import DateField from './date-field'
+import DateTimeField from './date-time-field'
+import NumberField from './number-field'
+import RadioButtons from './radio-buttons'
+import SelectBox from './select-box'
+import TextField from './text-field'
+import TextArea from './text-area'
 
 /**
  * Wrap a React class in with the common Container class
@@ -30,14 +30,14 @@ function wrapField (field, options = {}) {
  */
 function fields (options = {}) {
   return {
-    check_box: wrapField(CheckBox, options.check_box),
-    date_field: wrapField(DateField, options.date_field),
-    date_time_field: wrapField(DateTimeField, options.date_time_field),
-    number_field: wrapField(NumberField, options.number_field),
-    radio_buttons: wrapField(RadioButtons, options.radio_buttons),
-    select_box: wrapField(SelectBox, options.select_box),
-    text_area: wrapField(TextArea, options.text_area),
-    text_field: wrapField(TextField, options.text_field)
+    checkBox: wrapField(CheckBox, options.checkBox),
+    dateField: wrapField(DateField, options.dateField),
+    dateTimeField: wrapField(DateTimeField, options.dateTimeField),
+    numberField: wrapField(NumberField, options.numberField),
+    radioButtons: wrapField(RadioButtons, options.radioButtons),
+    selectBox: wrapField(SelectBox, options.selectBox),
+    textArea: wrapField(TextArea, options.textArea),
+    textField: wrapField(TextField, options.textField)
   }
 }
 
