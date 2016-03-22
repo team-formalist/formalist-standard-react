b6db56956bc363c3e1a747a6dcee4cd5413f62be
Chris Buttery <info@chrisbuttery.com>
2016-03-22 12:00:01 +1100

    Tests check for fake server response

--- test/fields/upload-to-S3.js
+++ test/fields/upload-to-S3.js
@@ -4,9 +4,17 @@ import { upload, preSign } from '../../src/components/fields/file-upload/upload-
 
 const token = ''
 const url = 'www.foo.com'
-const noOp = function (file, url, token, resolve, reject) {
-  resolve()
+let serverResponse = {
+  status: 200,
+  text: '{"foo": "bar"}'
 }
+
+const fakeXHRResponse = function (file, url, token, showProgress) {
+  return new Promise((resolve, reject) => {
+    resolve(serverResponse)
+  })
+}
+
 const file = {
   name: 'W1004855.jpg',
   lastModified: 1458086083000,
@@ -18,29 +26,77 @@ const file = {
 
 test('presign:', (nest) => {
   nest.test('...returns a promise', (t) => {
-    return preSign(file, url, token, noOp)
+    return preSign(file, url, token, fakeXHRResponse)
   })
 
   nest.test('...should fail', (t) => {
-    return t.shouldFail(preSign(file, url, token, noOp).then(() => {
-      throw new Error('Failed!')
-    }))
+    return t.shouldFail(preSign(file, url, token, fakeXHRResponse)
+      .then(() => {
+        throw new Error('Failed!')
+      }))
+  })
+
+  nest.test('...should return a response', (t) => {
+    preSign(file, url, token, fakeXHRResponse)
+      .then((res) => {
+        t.equal(res.foo, 'bar')
+        t.end()
+      })
+  })
+
+  nest.test('...should fail on 500', (t) => {
+    let failedStatus = Object.assign({}, serverResponse, {status: 500})
+
+    const fakeXHRResponse = function (file, url, token) {
+      return new Promise((resolve, reject) => {
+        resolve(failedStatus)
+      })
+    }
+
+    return t.shouldFail(preSign(file, url, token, fakeXHRResponse))
   })
 })
 
 test('upload:', (nest) => {
-  const response = {
+  const presignResponse = {
     url: 'foo',
     id: 1
   }
 
+  const showProgress = function (e) {}
+
+  serverResponse = {
+    status: 200,
+    text: '{"baz": "qux"}'
+  }
+
   nest.test('...returns a promise', (t) => {
-    return upload(response, file, token, noOp)
+    return upload(presignResponse, file, token, showProgress, fakeXHRResponse)
   })
 
   nest.test('...should fail', (t) => {
-    return t.shouldFail(upload(response, file, token, noOp).then(() => {
+    return t.shouldFail(upload(presignResponse, file, token, showProgress, fakeXHRResponse).then(() => {
       throw new Error('Failed!')
     }))
+  })
+
+  nest.test('...should return a response', (t) => {
+    upload(presignResponse, file, token, showProgress, fakeXHRResponse)
+      .then((res) => {
+        t.equal(res.baz, 'qux')
+        t.end()
+      })
+  })
+
+  nest.test('...should fail on 500', (t) => {
+    let failedStatus = Object.assign({}, serverResponse, {status: 500})
+
+    const fakeXHRResponse = function (file, url, token) {
+      return new Promise((resolve, reject) => {
+        resolve(failedStatus)
+      })
+    }
+
+    return t.shouldFail(upload(file, url, token, fakeXHRResponse))
   })
 })
