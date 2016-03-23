5373873a4f830d020561f4479836a0632e38356f
Max Wheeler <max@makenosound.com>
2016-03-23 21:48:06 +1100

    Validate fields as they’re edited.

--- src/components/fields/container.js
+++ src/components/fields/container.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { List } from 'immutable'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import classNames from 'classnames'
+import validation from 'formalist-validation'
 import styles from './container.mcss'
 import { actions } from 'formalist-compose'
 const { addField, deleteField, editField, validateField } = actions
@@ -51,6 +53,13 @@ const FieldContainer = React.createClass({
     // Turn the attributes from an Immutable.Map into a POJO
     attributes = attributes.toJS()
 
+    // Extract a few things from attributes
+    let label = attributes.label || this.props.name.replace(/_/g, ' ')
+    let { hint } = attributes
+
+    // Curry with the form validation schema
+    let validate = validation(attributes.validation)
+
     // Abstract the actions so that each field doesn't have to worry about
     // the action implementation
     let fieldActions = {
@@ -65,16 +74,18 @@ const FieldContainer = React.createClass({
         )
       },
       edit: (val) => {
+        let editedValue = val()
+        // Ensure we're not passing Immutable stuff through
+        // to the validator
+        if (List.isList(editedValue)) {
+          editedValue = editedValue.toJS()
+        }
         return store.batchDispatch([
           editField(path, val),
-          validateField(path, val)
+          validateField(path, validate(editedValue))
         ])
       }
     }
-
-    // Extract a few things from attributes
-    let label = attributes.label || this.props.name.replace(/_/g, ' ')
-    let hint = attributes.hint
 
     // Set up standard classNames
     let containerClassNames = classNames(
