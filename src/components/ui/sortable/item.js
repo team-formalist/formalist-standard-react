29c51cffd6bd1935e0e4f426e45c24953a5cb6b6
Chris Buttery <info@chrisbuttery.com>
2016-04-11 11:32:09 +1000

    Add data-name attr. Return click event on remove

--- src/components/ui/sortable/item.js
+++ src/components/ui/sortable/item.js
@@ -142,15 +142,16 @@ const Item = React.createClass({
   },
 
   /**
-   * Send current `index` to the onRemove callback
+   * Send current `index` and click event to the onRemove callback
    * @param  {Event} e Click event
    */
   onRemoveClick (e) {
     e.preventDefault()
     const { canRemove, onRemove } = this.props
     if (canRemove && onRemove) {
-      onRemove(this.props.index)
+      onRemove(this.props.index, e)
     }
+    ree
   },
 
   /**
@@ -176,7 +177,7 @@ const Item = React.createClass({
 
     return connectDropTarget(
       connectDragPreview(
-        <div className={styles.base} style={inline}>
+        <div className={styles.base} style={inline}  data-name="sortable-item">
           <div className={styles.inner}>
             {children}
           </div>
