071ea139302ea6546ea04c8e25956368ea1ef644
Max Wheeler <max@makenosound.com>
2017-10-23 08:13:54 +1100

    Fix reference to template

--- src/components/ui/rich-text-editor/block-toolbar-plugin/blocks/atomic/index.js
+++ src/components/ui/rich-text-editor/block-toolbar-plugin/blocks/atomic/index.js
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import template from "../../../../../";
+import template from "../../../../../../";
 import React from "react";
 import PropTypes from "prop-types";
 import { Entity } from "draft-js";
