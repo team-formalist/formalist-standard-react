3fa7715d167205ecb909542f59af3f0d0de74804
Andrew Croome <andrewcroome@gmail.com>
2020-11-27 17:31:37 +1100

    Add WIP component selection.

--- src/components/many-child-forms/index.js
+++ src/components/many-child-forms/index.js
@@ -2,13 +2,52 @@ import React from "react";
 import PropTypes from "prop-types";
 import ImmutablePropTypes from "react-immutable-proptypes";
 import classNames from "classnames";
+import fuzzy from "fuzzy";
 
 // Components
 import FieldErrors from "../fields/common/errors";
 import Sortable from "../ui/sortable";
+import Popout from "../ui/popout";
 
 // Styles
 import * as styles from "./styles";
+
+/**
+ * Default component for representing a "selected/selection" item
+ * @param  {Object} props Taking the shape of:
+ *
+ * {
+ *   option: { label: 'foo'}
+ *  }
+ *
+ * I.e., expecting the option to have a `label` key with a string value.
+ *
+ * @return {ReactElement}
+ */
+const SelectDefault = ({ option }) => (
+  <div className="fba-center" style={{ width: "100%" }}>
+    <div style={{ width: "calc(100% - 65px)" }}>
+      <p className="mb-xsmall">
+        {option.label}
+      </p>
+
+    </div>
+    <div style={{ width: "220px", marginLeft: "15px" }}>
+      {option.preview_image_url ? (
+        <img alt="" style={{ maxWidth: "100%" }} src={option.preview_image_url} />
+      ) : (
+        ""
+      )}
+    </div>
+  </div>
+);
+
+SelectDefault.propTypes = {
+  option: PropTypes.shape({
+    label: PropTypes.string,
+    preview_image_url: PropTypes.string
+  })
+};
 
 class ManyChildFormsSet extends React.Component {
   static propTypes = {
@@ -52,7 +91,9 @@ class ManyChildForms extends React.Component {
    * @return {Object}
    */
   state = {
-    contentsKey: Date.now()
+    contentsKey: Date.now(),
+    search: null,
+    searchFocus: false
   };
 
   componentWillReceiveProps(nextProps) {
@@ -72,7 +113,9 @@ class ManyChildForms extends React.Component {
     // We also check the `contentsKey` we set in state
     return (
       this.props.hashCode !== nextProps.hashCode ||
-      this.state.contentsKey !== nextState.contentsKey
+      this.state.contentsKey !== nextState.contentsKey ||
+      this.state.search !== nextState.search ||
+      this.state.searchFocus !== nextState.searchFocus
     );
   }
 
@@ -125,8 +168,114 @@ class ManyChildForms extends React.Component {
     this.updateContentsKey();
   };
 
+  /**
+   * When a selection is made, trigger change and close the selector
+   * @return {Null}
+   */
+  onFormSelection = formName => {
+    const { attributes, addChild } = this.props;
+    const form = attributes.get("embeddable_forms").get(formName)
+    addChild(formName, form);
+    this.updateContentsKey();
+  };
+
+  /**
+   * Open the selector popout
+   * @return {Null}
+   */
+  openSelector = () => {
+    this._selector.openPopout();
+  };
+
+  /**
+   * Close the selector popout
+   * @return {Null}
+   */
+  closeSelector = () => {
+    this._selector.closePopout();
+  };
+
+  /**
+   * On choose click, open selector
+   * @return {Null}
+   */
+  onChooseClick = e => {
+    e.preventDefault();
+    this.toggleSelector();
+  };
+
+  /**
+   * Toggle the selector popout
+   * @return {Null}
+   */
+  toggleSelector = () => {
+    this._selector.togglePopout();
+  };
+
+  /**
+   * On popout open, focus the search input
+   * @return {Null}
+   */
+  onPopoutOpen = () => {
+    this._search.focus();
+  };
+
+  /**
+   * On search input focus
+   * @param  {Event} e Keyboard event
+   * @return {Null}
+   */
+  onSearchFocus = e => {
+    this.setState({
+      searchFocus: true
+    });
+  };
+
+  /**
+   * On search input blur
+   * @param  {Event} e Keyboard event
+   * @return {Null}
+   */
+  onSearchBlur = e => {
+    this.setState({
+      searchFocus: false
+    });
+  };
+
+  /**
+   * Fired when search input is `change`d.
+   * Set this.state.search to the value of the input
+   * @param  {Event} e Keyboard event
+   * @return {Null}
+   */
+  onSearchChange = e => {
+    const search = e.target.value;
+    this.setState({
+      search: search
+    });
+  };
+
+  /**
+   * On popout close, reset the search
+   * @return {Null}
+   */
+  onPopoutClose = () => {
+    this.setState({
+      search: null
+    });
+  };
+
+  /**
+   * On popout open, focus the search input
+   * @return {Null}
+   */
+  onPopoutOpen = () => {
+    this._search.focus();
+  };
+
   render() {
     const { attributes, children, errors, name } = this.props;
+    const { search, searchFocus } = this.state;
     let hasErrors = errors.count() > 0;
     const { contentsKey } = this.state;
 
@@ -139,13 +288,94 @@ class ManyChildForms extends React.Component {
       [`${styles.labelErrors}`]: hasErrors
     });
 
+    let Option = SelectDefault;
+
+    const options = Object.keys(embeddable_forms).map(key => {
+      return {
+        id: key,
+        label: embeddable_forms[key].label,
+        preview_image_url: embeddable_forms[key].preview_image_url
+      }
+    });
+
+    // Filter options
+    let filteredOptions = options;
+    if (search) {
+      filteredOptions = options.filter(option => {
+        const values = [String(option["label"])];
+        const results = fuzzy.filter(search, values);
+        return results && results.length > 0;
+      });
+    }
+
+    // Build the set of options
+    const renderedOptions = filteredOptions.map(option => {
+      // TODO Asses whether to remove this binding
+      /* eslint-disable react/jsx-no-bind */
+      let onClick = function(e) {
+        e.preventDefault();
+        this.onFormSelection(option.id);
+      }.bind(this);
+      /* eslint-enable react/jsx-no-bind */
+      return (
+        <button
+          key={option.id}
+          className={styles.optionButton}
+          onClick={onClick}
+        >
+          <Option option={option} />
+        </button>
+      );
+    });
+
+    // TODO : clean up popup button - it's weirdly nested
+
     return (
       <div className={styles.base} data-many-child-forms={name}>
         <div className={styles.header}>
           <h3 className={labelClassNames}>{label}</h3>
           <div className={styles.controls}>
-            <button className={styles.addButton} onClick={this.addChild}>
-              {action_label || "Add item"}
+            <button
+              data-open-selector-button
+              className={styles.addButton}
+              onClick={this.onChooseClick}
+            >
+
+              <Popout
+                ref={r => {
+                  this._selector = r;
+                }}
+                placement="left"
+                onClose={this.onPopoutClose}
+                onOpen={this.onPopoutOpen}
+                closeOnEsc={!searchFocus || !search}
+                closeOnOutsideClick
+                testId={`selection-field:${name}`}
+              >
+                <div className={styles.openSelectorButton}>
+                  {action_label || "Add item"}
+                </div>
+                <div className={styles.options}>
+                  <input
+                    ref={r => {
+                      this._search = r;
+                    }}
+                    type="search"
+                    className={styles.search}
+                    placeholder="Type to filter"
+                    onBlur={this.onSearchBlur}
+                    onFocus={this.onSearchFocus}
+                    onChange={this.onSearchChange}
+                  />
+                  <div className={styles.optionsList}>
+                    {renderedOptions.length > 0 ? (
+                      renderedOptions
+                    ) : (
+                      <p className={styles.noResults}>No matching results</p>
+                    )}
+                  </div>
+                </div>
+              </Popout>
             </button>
           </div>
         </div>
@@ -169,12 +399,6 @@ class ManyChildForms extends React.Component {
             <span className={styles.placeholderText}>
               {placeholder || "No items have been added."}{" "}
             </span>
-            <button
-              className={styles.placeholderButton}
-              onClick={this.addChild}
-            >
-              Add the first?
-            </button>
           </div>
         )}
         {hasErrors ? <FieldErrors errors={errors} /> : null}
